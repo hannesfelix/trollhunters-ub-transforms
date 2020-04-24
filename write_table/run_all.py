4f5d35938e7c33c1c3311500ecf6e57ba78e15f7
Nandan Rao <nandanmarkrao@gmail.com>
2020-04-24 07:43:33 +0000

    changes in network for exploration

--- write_table/run_all.py
+++ write_table/run_all.py
@@ -1,11 +1,13 @@
 import subprocess
 
 tables = [
-    'tweets-2018-01',
-    'tweets-2018-02',
-    'tweets-2018-03',
-    'tweets-2018-04',
-    'tweets-2018-05'
+	'tweets-2019-01',
+	'tweets-2019-02',
+	'tweets-2019-03',
+	'tweets-2019-04',
+	'tweets-2019-05',
+	'tweets-2019-06',
+	'tweets-2019-07',
 ]
 
 for table in tables:
